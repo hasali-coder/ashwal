50e7d281cd14feafbee417c92c6335fe3db084a4
Ali M <58490554+hasali-coder@users.noreply.github.com>
2023-11-02 08:56:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,117 +2,128 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lipa na mpesa</title>
-    <link
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-      rel="stylesheet"
-    />
-    <link href="" rel="stylesheet" />
-    <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" ">
-    <script
-      type="text/javascript"
-      src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"
-    ></script>
+    <title>Lipa na M-Pesa</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <style>
-      @import url("https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap");
+        body {
+            background-color: #f4f4f4;
+            font-family: 'Century Gothic', sans-serif;
+        }
 
-      body {
-        background-color: #eaedf4;
-        font-family: "Rubik", sans-serif;
-      }
+        .card {
+            width: 400px;
+            border: none;
+            border-radius: 20px;
+            margin: auto;
+            padding: 30px;
+            background-image: url('kenya.jpg'); /* Replace with your pattern image */
+            background-size: cover;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        }
 
-      .card {
-        width: 310px;
-        border: none;
-        border-radius: 15px;
-      }
+        .options {
+            display: flex;
+            justify-content: space-around;
+            margin: 20px 0;
+        }
 
-      .justify-content-around div {
-        border: none;
-        border-radius: 20px;
-        background: #f3f4f6;
-        padding: 5px 20px 5px;
-        color: #8d9297;
-      }
+        .option {
+            border: none;
+            border-radius: 20px;
+            background: #e57f10;
+            padding: 10px 20px;
+            color: #fff;
+            font-size: 18px;
+            cursor: pointer;
+        }
 
-      .justify-content-around span {
-        font-size: 12px;
-      }
+        .option:hover {
+            background: #f3952a;
+        }
 
-      .justify-content-around div:hover {
-        background: #545ebd;
-        color: #fff;
-        cursor: pointer;
-      }
+        .mpesa {
+            background: #00796b;
+        }
 
-      .justify-content-around div:nth-child(1) {
-        background: #545ebd;
-        color: #fff;
-      }
+        .media {
+            padding: 20px;
+            display: flex;
+            align-items: center;
+        }
 
-      span.mt-0 {
-        color: #8d9297;
-        font-size: 12px;
-      }
+        .media img {
+            border-radius: 15px;
+            width: 100px;
+            height: 100px;
+        }
 
-      h6 {
-        font-size: 15px;
-      }
-      .mpesa {
-        background-color: green !important;
-      }
+        .media h6 {
+            font-size: 22px;
+            color: #e57f10;
+            margin-left: 20px;
+        }
 
-      img {
-        border-radius: 15px;
-      }
+        .form-label {
+            color: #e57f10;
+            font-weight: 600;
+        }
+
+        .form-control {
+            border: 1px solid #ccc;
+            border-radius: 10px;
+            padding: 15px;
+            font-size: 16px;
+        }
+
+        .btn-success {
+            background-color: #e57f10;
+            color: #fff;
+            border: none;
+            border-radius: 10px;
+            padding: 15px 30px;
+            font-weight: 600;
+            cursor: pointer;
+        }
+
+        .btn-success:hover {
+            background-color: #f3952a;
+        }
     </style>
-  </head>
-  <body oncontextmenu="return false" class="snippet-body">
-    <div class="container d-flex justify-content-center">
-      <div class="card mt-5 px-3 py-4">
-        <div class="d-flex flex-row justify-content-around">
-          <div class="mpesa"><span>Mpesa </span></div>
-          <div><span>Paypal</span></div>
-          <div><span>Card</span></div>
-        </div>
-        <div class="media mt-4 pl-2">
-          <img src="./images/1200px-M-PESA_LOGO-01.svg.png" class="mr-3" height="75" />
-          <div class="media-body">
-            <h6 class="mt-1">Enter Amount & Number</h6>
-          </div>
-        </div>
-        <div class="media mt-3 pl-2">
-                          <!--bs5 input-->
-
-            <form class="row g-3" action="./stk_initiate.php" method="POST">
-            
-                <div class="col-12">
-                  <label for="inputAddress" class="form-label">Amount</label>
-                  <input type="text" class="form-control" name="amount" placeholder="Enter Amount">
+</head>
+<body oncontextmenu="return false" class="snippet-body">
+    <div class="container">
+        <div class="card mt-5">
+            <div class="options">
+                <div class="option mpesa"><span>M-Pesa</span></div>
+                <div class="option"><span>Paypal</span></div>
+                <div class="option"><span>E-Card</span></div>
+            </div>
+            <div class="media">
+            <img src="./images/1200px-M-PESA_LOGO-01.svg.png" class="mr-3" height="75" />
+                <div class="media-body">
+                    <h6 class="mt-1">Enter Amount & Number</h6>
                 </div>
-                <div class="col-12">
-                  <label for="inputAddress2" class="form-label" >Phone Number</label>
-                  <input type="text" class="form-control" name="phone"  placeholder="Enter Phone Number">
-                </div>
-             
-                <div class="col-12">
-                  <button type="submit" class="btn btn-success" name="submit" value="submit">Donate</button>
-                </div>
-              </form>
-              <!--bs5 input-->
-          </div>
+            </div>
+            <div class="media">
+                <form class="row g-3" action="./stk_initiate.php" method="POST">
+                    <div class="col-12">
+                        <label for="amount" class="form-label">Amount</label>
+                        <input type="text" class="form-control" name="amount" id="amount" placeholder="Enter Amount">
+                    </div>
+                    <div class="col-12">
+                        <label for="phone" class="form-label">Phone Number</label>
+                        <input type="text" class="form-control" name="phone" id="phone" placeholder="Enter Phone Number">
+                    </div>
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-success" name="submit" value="submit">Donate</button>
+                    </div>
+                </form>
+            </div>
         </div>
-      </div>
     </div>
-    <script
-      type="text/javascript"
-      src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"
-    ></script>
-    <script type="text/javascript" src=""></script>
-    <script type="text/javascript" src=""></script>
-    <script type="text/Javascript"></script>
-  </body>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+</body>
 </html>
